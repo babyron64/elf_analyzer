8f8fff2de6f78425a647c4c2a1c6e2fb258be248
babyron64 <babyron64@gmail.com>
2017-12-15 16:12:05 +0000

    add relp functions (incomplete)

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ FLAGS=-I ./include -g -O0
 MAIN=$(SRC)/main.c
 LOAD=$(SRC)/elf_load.c
 
-OBJS=$(BIN)/main.o $(BIN)/load.o $(BIN)/repl.o
+OBJS=$(BIN)/main.o $(BIN)/load.o $(BIN)/repl.o $(BIN)/cmd.o
 OBJS+=$(BIN)/elfhdr.o $(BIN)/segment.o $(BIN)/section.o $(BIN)/shstr.o
 
 OUTPUT=./analyelf.out
--- src/repl.c
+++ src/repl.c
@@ -1,17 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <linux/elf.h>
 
+#include "analy_ctrl.h"
 #include "elf_analyzer.h"
 
-int print_info();
+#define MAX_TOK_NUM 4
+
+static int loop();
+static int read_toks(Tok *toks, char *inputs);
+static Tok get_tok(const char *cmd);
+static int print_info();
 
 int
 repl() {
-    print_info();
+    loop();
     return 0;
 }
 
-int
+static int
+loop() {
+    char cmd[128];
+    Tok toks[MAX_TOK_NUM];
+    int res;
+
+    while (1) {
+        printf("(elf_analyzer) ");
+        scanf("%s", cmd);
+        read_toks(toks, cmd);
+        res = eval(toks);
+        if (res == 1)
+            break;
+        if (res == -1)
+            fprintf(stderr, "command execution failure");
+    }
+
+    return 0;
+}
+
+static int
+read_toks(Tok *toks, char *inputs) {
+    char *p = strtok(inputs, " ");
+    Tok tok;
+
+    int i = 0;
+    while (1) {
+        if (p == NULL || i == MAX_TOK_NUM)
+            break;
+        tok = get_tok(p);
+        if (tok == -1)
+            return -1;
+        toks[i++] = tok;
+        p = strtok(NULL, " ");
+    }
+
+    return 0;
+}
+
+static Tok
+get_tok(const char *cmd) {
+    int num = atoi(cmd);
+    if (0 < num && num < MAX_NUMERIC)
+        return num;
+    else if (strcmp(cmd, "0") == 0)
+        return 0;
+    else if (strcmp(cmd, "quit") == 0)
+        return QUIT;
+    else if (strcmp(cmd, "ehdr") == 0)
+        return EHDR;
+    else if (strcmp(cmd, "phdr") == 0)
+        return PHDR;
+    else if (strcmp(cmd, "shdr") == 0)
+        return SHDR;
+    else if (strcmp(cmd, "seg") == 0)
+        return SEG;
+    else if (strcmp(cmd, "sec") == 0)
+        return SEC;
+    return -1;
+}
+
+static int
 print_info() {
     const Elf64_Ehdr *p_ehdr64 = get_ehdr();
     const Elf64_Shdr *p_shstr64 = get_shstr();
@@ -20,5 +89,6 @@ print_info() {
     char buf[16];
     read_sec_name(buf, p_shstr64, 16);
     printf("string section name: %s\n",buf);
+    print_ehdr();
     return 0;
 }
