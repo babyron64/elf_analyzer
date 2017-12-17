be5e64b45aa60bfd5f90335e86d32826b9177408
babyron64 <babyron64@gmail.com>
2017-12-17 19:44:40 +0000

    Remove: delete un-necessary type declaration

--- include/analy_cmd.h
+++ include/analy_cmd.h
@@ -4,8 +4,6 @@
 #define MAX_CMD_LEN 8
 #define MAX_TOK_NUM 8
 
-typedef char CMD_ARGS[][MAX_CMD_LEN];
-
 #define CMD_CALL(cmd, cmdc, ix, cmds) \
     (cmdc)-1 > 0?cmd((cmdc)-1, (ix)+1, cmds):cmd(0, 0, NULL)
 
